ac0e11ea9847ecaab3bb8ea60da2450e1cb6d545
Sebastian Thomas Van Den Dungen <u5561028@anu.edu.au>
2018-05-21 23:49:11 +1000

    updating to current status

--- site.js
+++ site.js
@@ -21,3 +21,52 @@ function openTab(evt, tabname) {
     document.getElementById(tabname).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+/* 
+    Functions for the lightbox effect
+    sourced from: https://www.w3schools.com/howto/howto_js_lightbox.asp
+*/
+
+// Open the Modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// show the slide at the index n.
+// Iterates through all the slides setting their display to inactive, then sets the visible set of n 
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("thumbnail");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
